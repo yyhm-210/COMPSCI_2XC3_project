02d659381392ef4e0dec20ce40ae687d2d2183e8
Tony He <hetianyan666@gmail.com>
2026-04-02 00:23:57 -0400

    tidied

--- final_project_part1.py
+++ final_project_part1.py
@@ -42,6 +42,7 @@ def dijkstra(G, source):
     for node in nodes:
         Q.insert(min_heap.Element(node, float("inf")))
         dist[node] = float("inf")
+    dist[source] = 0
     Q.decrease_key(source, 0)
 
     #Meat of the algorithm
@@ -50,12 +51,47 @@ def dijkstra(G, source):
         current_node = current_element.value
         dist[current_node] = current_element.key
         for neighbour in G.adj[current_node]:
-            if dist[current_node] + G.w(current_node, neighbour) < dist[neighbour]:
-                Q.decrease_key(neighbour, dist[current_node] + G.w(current_node, neighbour))
-                dist[neighbour] = dist[current_node] + G.w(current_node, neighbour)
+            if neighbour in Q.map and dist[current_node] + G.w(current_node, neighbour) < dist[neighbour]:
+                new_dist = dist[current_node] + G.w(current_node, neighbour)
+                Q.decrease_key(neighbour, new_dist)
+                dist[neighbour] = new_dist
                 pred[neighbour] = current_node
     return dist
 
+def dijkstra_approx(G, source, k):
+    pred = {}
+    dist = {}
+    relax_count = {}
+    Q = min_heap.MinHeap([])
+    nodes = list(G.adj.keys())
+
+    for node in nodes:
+        Q.insert(min_heap.Element(node, float("inf")))
+        dist[node] = float("inf")
+        relax_count[node] = 0
+
+    dist[source] = 0
+    Q.decrease_key(source, 0)
+
+    while not Q.is_empty():
+        current_element = Q.extract_min()
+        current_node = current_element.value
+        dist[current_node] = current_element.key
+
+        for neighbour in G.adj[current_node]:
+            new_dist = dist[current_node] + G.w(current_node, neighbour)
+
+            if (
+                neighbour in Q.map
+                and new_dist < dist[neighbour]
+                and relax_count[neighbour] < k
+            ):
+                Q.decrease_key(neighbour, new_dist)
+                dist[neighbour] = new_dist
+                relax_count[neighbour] += 1
+                pred[neighbour] = current_node
+
+    return dist
 
 def bellman_ford(G, source):
     pred = {} #Predecessor dictionary. Isn't returned, but here for your understanding
@@ -70,17 +106,57 @@ def bellman_ford(G, source):
     #Meat of the algorithm
     for _ in range(G.number_of_nodes()):
         for node in nodes:
+            if dist[node] == float("inf"):
+                continue
             for neighbour in G.adj[node]:
                 if dist[neighbour] > dist[node] + G.w(node, neighbour):
                     dist[neighbour] = dist[node] + G.w(node, neighbour)
                     pred[neighbour] = node
     return dist
 
+def bellman_ford_approx(G, source, k):
+    pred = {}
+    dist = {}
+    relax_count = {}
+    nodes = list(G.adj.keys())
+
+    for node in nodes:
+        dist[node] = float("inf")
+        relax_count[node] = 0
+    dist[source] = 0
+
+    for _ in range(G.number_of_nodes()):
+        updated = False
+
+        for node in nodes:
+            if dist[node] == float("inf"):
+                continue
+
+            for neighbour in G.adj[node]:
+                new_dist = dist[node] + G.w(node, neighbour)
+
+                if new_dist < dist[neighbour] and relax_count[neighbour] < k:
+                    dist[neighbour] = new_dist
+                    relax_count[neighbour] += 1
+                    pred[neighbour] = node
+                    updated = True
+
+        if not updated:
+            break
+
+    return dist
 
 def total_dist(dist):
     total = 0
     for key in dist.keys():
         total += dist[key]
+    return total
+
+def finite_total_dist(dist):
+    total = 0
+    for value in dist.values():
+        if value != float("inf"):
+            total += value
     return total
 
 def create_random_complete_graph(n,upper):
@@ -93,6 +169,22 @@ def create_random_complete_graph(n,upper):
                 G.add_edge(i,j,random.randint(1,upper))
     return G
 
+def create_random_density_graph(n, density, upper):
+    G = DirectedWeightedGraph()
+    for i in range(n):
+        G.add_node(i)
+    for i in range(n - 1):
+        G.add_edge(i, i + 1, random.randint(1, upper))
+    max_edges = n * (n - 1)
+    target_edges = int(density * max_edges)
+    current_edges = len(G.weights)
+    while current_edges < target_edges:
+        u = random.randint(0, n - 1)
+        v = random.randint(0, n - 1)
+        if u != v and not G.are_connected(u, v):
+            G.add_edge(u, v, random.randint(1, upper))
+            current_edges += 1
+    return G
 
 #Assumes G represents its nodes as integers 0,1,...,(n-1)
 def mystery(G):
